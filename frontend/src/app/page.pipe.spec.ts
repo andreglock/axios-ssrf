da162db265a81d7e238917cf420fcecc43a4e9aa
andreglock <andre.glock@mementor.de>
2025-04-04 14:05:49 +0200

    Add more test cases for page.pipe.spec.

--- frontend/src/app/page.pipe.spec.ts
+++ frontend/src/app/page.pipe.spec.ts
@@ -5,4 +5,16 @@ describe('PagePipe', () => {
     const pipe = new PagePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return the page number when it is not -1', () => {
+    const pipe = new PagePipe();
+    const result = pipe.transform(2);
+    expect(result).toBe(2);
+  });
+
+  it('should return ... when it is -1', () => {
+    const pipe = new PagePipe();
+    const result = pipe.transform(-1);
+    expect(result).toBe('...');
+  });
 });
