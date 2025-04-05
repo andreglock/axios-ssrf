64177071c7081f379b8906efd8ec51ae337eb770
andreglock <andreglock@gmail.com>
2025-04-05 09:47:11 +0200

    Add deploy scripts.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { NextFunction, Response } from 'express';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'error', 'warn', 'log', 'verbose'],
   });
   app.setGlobalPrefix('api');
-  app.enableCors();
 
   app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader('X-Frame-Options', 'deny');
@@ -24,6 +23,7 @@ async function bootstrap() {
     next();
   });
 
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 
   Logger.debug(`Application is running on: ${await app.getUrl()}`);
