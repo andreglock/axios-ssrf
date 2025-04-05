c213292596f02951f0724c580a2cd36c83e08078
andreglock <andreglock@gmail.com>
2025-04-05 10:06:52 +0200

    Adjust port.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   });
 
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 
   Logger.debug(`Application is running on: ${await app.getUrl()}`);
 }
